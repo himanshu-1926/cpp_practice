83e036c43a4e97ace3fa7263164f010955e44f57
Himanshu Yadav <iamhimanshu1926>
2024-11-21 11:54:21 +0530

    add inline prg

---
+++ tut17_inline.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ inline int product(int a , int b){
+    return a*b;
+
+}
+int main(){
+    int a , b;
+
+    cout<<"enter value of a, b"<<endl;
+    cin>>a>>b;
+ cout<<"the product is : "<<product(a,b)<<endl;
+ cout<<"the product is : "<<product(a,b)<<endl;
+ cout<<"the product is : "<<product(a,b)<<endl;
+ cout<<"the product is : "<<product(a,b)<<endl;
+ cout<<"the product is : "<<product(a,b)<<endl;
+
+}
